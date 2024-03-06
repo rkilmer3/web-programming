e2293e546f1681263bfa97c1a19aad4f8ac6cd39
Greg DeLozier <gregdelozier@gmail.com>
2024-03-06 17:56:58 -0500

    added redirect comment

--- topic-06-forms/hello.py
+++ topic-06-forms/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, #redirect
 
 app = Flask(__name__)
 
@@ -20,4 +20,5 @@ def post_hello():
         "password":password
     }
     return render_template("hello.html", data=data)
+    # usually do a redirect('....')
 
