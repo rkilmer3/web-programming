1c5d594ed1f940c31e7ca6b7d7bca9f76b3f6fd6
brendontn16 <brendontn16@gmail.com>
2024-05-08 13:46:13 -0400

    Comment Stuff

--- Final Project/create-quotes-db.py
+++ Final Project/create-quotes-db.py
@@ -24,3 +24,41 @@ quotes_collection.insert_many(quotes_data)
 
 # make sure the quotes are there
 print(quotes_collection.count_documents({}))
+
+###################
+# Create a comments database
+comments_db = client.comments_db
+
+# Create a comments collection
+comments_collection = comments_db.comments_collection
+
+# Empty the collection
+comments_collection.delete_many({})
+
+# Sample comments data
+comments_data = [
+    {"text": "This is a comment.", "user": "Alice", "quote_id": 1},
+    {"text": "This is another comment.", "user": "Bob", "quote_id": 2},
+]
+
+# Put the comments in the database
+comments_collection.insert_many(comments_data)
+
+# Make sure the comments are there
+print(comments_collection.count_documents({}))
+
+# Function to add a comment to a quote
+def add_comment(quote_id, comment):
+    quote = quotes_collection.find_one({"_id": quote_id})
+    if quote and not quote.get('comments_prohibited', False):
+        comments_collection.insert_one(comment)
+    else:
+        print("Comments are prohibited on this quote.")
+
+# Function to prohibit comments on a quote
+def prohibit_comments(quote_id):
+    quotes_collection.update_one({"_id": quote_id}, {"$set": {"comments_prohibited": True}})
+
+# Function to delete a comment
+def delete_comment(comment_id):
+    comments_collection.delete_one({"_id": comment_id})
